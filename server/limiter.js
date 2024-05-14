c9130b8f734ea13ccc076c88510662a2cd42a900
novice <seungyeon5374@gmail.com>
2024-05-14 17:52:02 +0900

    제한

--- server/limiter.js
+++ server/limiter.js
@@ -1,7 +1,7 @@
 const expressRateLimit = require('express-rate-limit')
 
 const limitUsage = expressRateLimit({
-    windowMs: 60 * 1000, // 1분 (ms)
+    windowMs: 1 * 60 * 1000, // 1분 (ms)
     max: 100, //분당 최대 사용횟수
     handler(req,res){
         res.status(429).json({
